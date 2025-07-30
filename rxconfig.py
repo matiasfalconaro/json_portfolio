6ab3e6d28e37a193e87d82798e1d850c1606a7dd
Matías Falconaro <mlfalconaro@gmail.com>
2025-07-30 01:46:33 -0300

    Fix port

--- rxconfig.py
+++ rxconfig.py
@@ -4,7 +4,7 @@ import reflex as rx
 config = rx.Config(
     app_name="portfolio",
     api_url=f"https://mfalconaro.onrender.com",
-    port=int(os.environ.get("PORT", 3000)),
+    port=int(os.environ.get("PORT", 8000)),
     host="0.0.0.0",
     tailwind=None,
     disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"]
