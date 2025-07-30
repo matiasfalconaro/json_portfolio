2e3d723924b5ae9b35885797089420f0c7382df3
Matías Falconaro <mlfalconaro@gmail.com>
2025-07-30 01:41:48 -0300

    Update domain

--- rxconfig.py
+++ rxconfig.py
@@ -3,8 +3,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="portfolio",
-    api_url=f"https://json-portfolio.onrender.com",
-    port=int(os.environ.get("PORT", 8000)),
+    api_url=f"https://mfalconaro.onrender.com",
+    port=int(os.environ.get("PORT", 3000)),
     host="0.0.0.0",
     tailwind=None,
     disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"]
