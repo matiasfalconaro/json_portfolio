569274dbddeb3260775a305c7e6cc8fa0e422d5c
Matías Falconaro <mlfalconaro@gmail.com>
2025-07-30 00:29:59 -0300

    Change websocket port

--- rxconfig.py
+++ rxconfig.py
@@ -3,7 +3,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="portfolio",
-    port=int(os.environ.get("PORT", 3000)),
+    port=int(os.environ.get("PORT", 8000)),
     tailwind=None,
     disable_plugins=['reflex.plugins.sitemap.SitemapPlugin']
 )
