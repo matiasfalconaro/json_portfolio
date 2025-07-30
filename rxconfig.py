889853ab55509c19c4f8f52c789e17b12940895d
Matías Falconaro <mlfalconaro@gmail.com>
2025-07-30 00:22:08 -0300

    Update rxconfig

--- rxconfig.py
+++ rxconfig.py
@@ -1,6 +1,9 @@
+import os
 import reflex as rx
 
 config = rx.Config(
     app_name="portfolio",
+    port=int(os.environ.get("PORT", 3000)),
     tailwind=None,
+    disable_plugins=['reflex.plugins.sitemap.SitemapPlugin']
 )
