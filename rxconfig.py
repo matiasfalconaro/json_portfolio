f78feebc809f0b17d8a0441a2f84419d75fcb729
Matías Falconaro <mlfalconaro@gmail.com>
2025-07-30 01:14:46 -0300

    Add API-url for websockets

--- rxconfig.py
+++ rxconfig.py
@@ -3,8 +3,9 @@ import reflex as rx
 
 config = rx.Config(
     app_name="portfolio",
+    api_url=f"https://json-portfolio.onrender.com",
     port=int(os.environ.get("PORT", 8000)),
     host="0.0.0.0",
     tailwind=None,
-    disable_plugins=['reflex.plugins.sitemap.SitemapPlugin'],
+    disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"]
 )
