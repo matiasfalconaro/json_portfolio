091591e410b2a0ae8a748a365b30cf2d8d03aada
Matías Falconaro <mlfalconaro@gmail.com>
2025-07-30 00:35:41 -0300

    Hot fix - Websockets

--- rxconfig.py
+++ rxconfig.py
@@ -4,6 +4,7 @@ import reflex as rx
 config = rx.Config(
     app_name="portfolio",
     port=int(os.environ.get("PORT", 8000)),
+    host="0.0.0.0",
     tailwind=None,
-    disable_plugins=['reflex.plugins.sitemap.SitemapPlugin']
+    disable_plugins=['reflex.plugins.sitemap.SitemapPlugin'],
 )
