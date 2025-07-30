9d0cdffdaa8b3a568526a936356a6e91e606aeb8
Matías Falconaro <mlfalconaro@gmail.com>
2025-07-30 02:17:41 -0300

    Remove API URL

--- rxconfig.py
+++ rxconfig.py
@@ -3,8 +3,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="portfolio",
-    api_url=f"https://mfalconaro.onrender.com:8000",
-    port=int(os.environ.get("PORT", 8000)),
+    port=int(os.environ.get("PORT", 3000)),
     host="0.0.0.0",
     tailwind=None,
     disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"]
