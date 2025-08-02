486af3af16bd8effe7292d6509c8e340e82e655e
Matías Falconaro <mlfalconaro@gmail.com>
2025-08-02 20:31:08 -0300

    Add configuration .toml file

--- rxconfig.py
+++ rxconfig.py
@@ -1,6 +1,11 @@
+import os
 import reflex as rx
 
 config = rx.Config(
     app_name="portfolio",
-    tailwind=None,
+    port=int(os.environ.get("PORT", 8000)),
+    proxy_headers=True,
+    api_url=os.environ.get("API_URL", "https://mfalconaro.up.railway.app"),
+    disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"],
+    tailwind=None
 )
