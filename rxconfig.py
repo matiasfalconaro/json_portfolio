89c1942dbe706f0d74329f1c7c63de55caac5da3
Matías Falconaro <mlfalconaro@gmail.com>
2025-08-02 22:02:47 -0300

    Simplify rxconfig

--- rxconfig.py
+++ rxconfig.py
@@ -1,12 +1,6 @@
-import os
 import reflex as rx
-
 
 config = rx.Config(
     app_name="portfolio",
-    port=int(os.environ.get("PORT", 8000)),
-    proxy_headers=True,
-    api_url=os.environ.get("API_URL", "https://mfalconaro.up.railway.app"),
-    disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"],
-    tailwind=None
+    tailwind=None,
 )
