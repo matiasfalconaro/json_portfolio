2d945e10f9b97f9058449460b3ead1c82f6d47a7
Matías Falconaro <mlfalconaro@gmail.com>
2025-09-13 02:22:29 -0300

    Hot Fic - v1.3.3

--- portfolio/components.py
+++ portfolio/components.py
@@ -96,7 +96,7 @@ def code_info_modal() -> rx.Component:
                 rx.vstack(
                     rx.hstack(
                         rx.icon(tag="git-branch"),
-                        rx.text("v1.3.1"
+                        rx.text("v1.3.2"
                         ""),
                         align_items="center"
                     ),
